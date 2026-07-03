30bfc3f336e5cf28bb945896507043319e753b10
Muskan-19 <100256829+Muskan-19@users.noreply.github.com>
2026-07-03 07:20:03 +0530

    Create  Task.java

---
+++ Task.java
@@ -0,0 +1,50 @@
+import java.util.List;
+
+public class Task {
+
+    private String id;
+    private int priority;
+
+    private int cpu;
+    private int ram;
+    private int gpu;
+    private int network;
+
+    private List<Integer> slaWindow;
+    private List<String> conflicts;
+
+    public Task() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public int getCpu() {
+        return cpu;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public int getGpu() {
+        return gpu;
+    }
+
+    public int getNetwork() {
+        return network;
+    }
+
+    public List<Integer> getSlaWindow() {
+        return slaWindow;
+    }
+
+    public List<String> getConflicts() {
+        return conflicts;
+    }
+}
